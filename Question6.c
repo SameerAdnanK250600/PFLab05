89c301035ada2d92a1faa25732beee88d069ed22
Muhammad Sameer Adnan <k250600@nu.edu.pk>
2025-09-27 20:28:36 +0500

    Update Question6.c

--- Question6.c
+++ Question6.c
@@ -1,21 +1,38 @@
-// CHANGE THIS TO PRINT BINARY ASWELL, WILL DO
-
-#include <stdio.h>
-
-int main() {	
-	int a, b;
-
-	printf("Enter Num A: ");
-	scanf("%d", &a);
-	
-	printf("Enter Num B: ");
-	scanf("%d", &b);
-	
-	printf("Evaluating (a & b) | (a ^ b)\n");
-	
-	printf("(a & b) =  %d\n", (a&b));
-	printf("(a ^ b) = %d\n", (a^b));
-	printf("(a&b) | (a^b)) = %d\n", (a & b) | (a ^ b));
-		
-	return 0;
+#include <stdio.h> 
+ 
+void binary(int n) { 
+    if (n > 1) { 
+        binary(n / 2); 
+    } 
+    printf("%d", n % 2); 
+} 
+ 
+int main() { 
+    int a, b; 
+ 
+    printf("Enter Num A: "); 
+    scanf("%d", &a); 
+ 
+    printf("Enter Num B: "); 
+    scanf("%d", &b); 
+ 
+    printf("\nEvaluating (a & b) | (a ^ b)\n\n"); 
+ 
+    int andRes = (a & b); 
+    int xorRes = (a ^ b); 
+    int finalRes = (a & b) | (a ^ b); 
+ 
+    printf("(a & b) = %d\t(binary: ", andRes); 
+    binary(andRes); 
+    printf(")\n"); 
+ 
+    printf("(a ^ b) = %d\t(binary: ", xorRes); 
+    binary(xorRes); 
+    printf(")\n"); 
+ 
+    printf("(a & b) | (a ^ b) = %d\t(binary: ", finalRes); 
+    binary(finalRes); 
+    printf(")\n"); 
+ 
+    return 0; 
 }
